c6e15c65f30f9104fe014e1be83c5f7228ee1ec4
Evandro Alfieri <62438551+evandroAlfieri@users.noreply.github.com>
2025-10-27 16:50:54 +0100

    Update variables

--- supabase/functions/bulk-import/index.ts
+++ supabase/functions/bulk-import/index.ts
@@ -173,8 +173,8 @@ Deno.serve(async (req) => {
 
   try {
     const supabase = createClient(
-      Deno.env.get('SUPABASE_URL') ?? '',
-      Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? ''
+      Deno.env.get('PROJECT_URL') ?? '',
+      Deno.env.get('SERVICE_ROLE_KEY') ?? ''
     );
 
     const { csvData } = await req.json();
